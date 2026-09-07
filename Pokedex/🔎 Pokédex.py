634f4e43e946d2aeddcc786efae3394c36fb5152
Resfort <ssgas007@gmail.com>
2026-09-07 22:35:11 +0300

    Update 🔎 Pokédex.py

--- Pokedex/🔎 Pokédex.py
+++ Pokedex/🔎 Pokédex.py
@@ -8,16 +8,17 @@ class Pokemon:
         self.base_exp = base_exp
     def get_pokemon_info(self):
         return f"{self.name} is a {self.poke_type}-type Pokémon with an attack stat of {self.attack} and base experience of {self.base_exp}."
-    def get_pokemon(self,name):
-        url = f"https://pokeapi.co/api/v2/pokemon/{name.lower()}"
-        response = requests.get(url)
-        data = response.json()
+def get_pokemon(name):
+    url = f"https://pokeapi.co/api/v2/pokemon/{name.lower()}"
+    response = requests.get(url)
+    data = response.json()
 
-        pokemon_obj = Pokemon(
-            data["name"],
-            data["types"][0]["type"]["name"],
-            data["stats"][1]["base_stat"],
-            data["base_experience"]
-        )     
-        return pokemon_obj
+    pokemon_obj = Pokemon(
+        data["name"],
+        data["types"][0]["type"]["name"],
+        data["stats"][1]["base_stat"],
+        data["base_experience"]
+    )     
+    return pokemon_obj
 
+print(get_pokemon("pikachu").get_pokemon_info())

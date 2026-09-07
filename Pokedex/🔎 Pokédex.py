5ec620860e5e0bce1f6aaa26d2e4d75a010ee7e5
Resfort <ssgas007@gmail.com>
2026-09-07 21:23:07 +0300

    Update 🔎 Pokédex.py

--- Pokedex/🔎 Pokédex.py
+++ Pokedex/🔎 Pokédex.py
@@ -12,3 +12,18 @@ data = response.json()
 print("Имя:", data["name"])
 print("Рост:", data["height"])
 print("Вес:", data["weight"])
+
+class Pokemon:
+    def __init__(self, name, poke_type, attack, base_exp):
+        self.name = name
+        self.poke_type = poke_type
+        self.attack = attack
+        self.base_exp = base_exp
+    def get_pokemon_info(self):
+        return f"{self.name} is a {self.poke_type}-type Pokémon with an attack stat of {self.attack} and base experience of {self.base_exp}."
+pikachu = Pokemon(
+    data["name"],
+    data["types"][0]["type"]["name"],
+    data["stats"][1]["base_stat"],
+    data["base_experience"]
+)

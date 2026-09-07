51b95ba31a6d7ce110ba798703156fcfa2d4a0f8
Resfort <ssgas007@gmail.com>
2026-09-07 21:45:14 +0300

    Update 🔎 Pokédex.py

--- Pokedex/🔎 Pokédex.py
+++ Pokedex/🔎 Pokédex.py
@@ -1,18 +1,5 @@
 import requests
 
-# Отправляем запрос к PokeAPI
-response = requests.get("https://pokeapi.co/api/v2/pokemon/pikachu")
-
-# Печатаем статус-код ответа (200 значит, что всё прошло успешно!)
-print("Статус ответа:", response.status_code)
-
-data = response.json()
-
-# Мы можем обращаться к ключам как в обычном словаре:
-print("Имя:", data["name"])
-print("Рост:", data["height"])
-print("Вес:", data["weight"])
-
 class Pokemon:
     def __init__(self, name, poke_type, attack, base_exp):
         self.name = name
@@ -21,9 +8,16 @@ class Pokemon:
         self.base_exp = base_exp
     def get_pokemon_info(self):
         return f"{self.name} is a {self.poke_type}-type Pokémon with an attack stat of {self.attack} and base experience of {self.base_exp}."
-pikachu = Pokemon(
-    data["name"],
-    data["types"][0]["type"]["name"],
-    data["stats"][1]["base_stat"],
-    data["base_experience"]
-)
+    def get_pokemon(self,name):
+        url = f"https://pokeapi.co/api/v2/pokemon/{name.lower()}"
+        response = requests.get(url)
+        data = response.json()
+
+        pokemon_obj = Pokemon(
+            data["name"],
+            data["types"][0]["type"]["name"],
+            data["stats"][1]["base_stat"],
+            data["base_experience"]
+        )     
+        return pokemon_obj
+

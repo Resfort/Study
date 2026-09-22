c4698bcc128382895e7a34056faed7f1387da50d
Resfort <ssgas007@gmail.com>
2026-09-22 17:09:50 +0500

    Update gues_num.py

--- Practice/gues_num.py
+++ Practice/gues_num.py
@@ -4,3 +4,20 @@ def guess_number():
     number_to_guess = random.randint(1, 100)
     attempts = 0
     guessed = False
+    while not guessed:
+        user_guess = int(input("Guess a number between 1 and 100: "))
+        attempts += 1
+        if user_guess < number_to_guess:
+            print("Too low! Try again.")
+        elif user_guess > number_to_guess:
+            print("Too high! Try again.")
+        else:
+            guessed = True
+            print(f"Congratulations! You've guessed the number {number_to_guess} in {attempts} attempts.")
+
+print("Welcome to the Number Guessing Game!")
+
+print("You have to guess a number between 1 and 100.")
+
+input("Press Enter to start the game...")
+

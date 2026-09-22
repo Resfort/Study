3f0ec3990982941bb181d0bfad26537b37053dda
Resfort <ssgas007@gmail.com>
2026-09-22 17:01:55 +0500

    Update 🔎 Pokédex.py

--- Pokedex/🔎 Pokédex.py
+++ Pokedex/🔎 Pokédex.py
@@ -1,11 +1,42 @@
-class Pokedex:
+import requests
 
+class Pokemon:
+    def __init__(self, name, poke_type, attack, base_exp):
+        self.name = name
+        self.poke_type = poke_type
+        self.attack = attack
+        self.base_exp = base_exp
+
+    def get_pokemon_info(self):
+        return f"{self.name} is a {self.poke_type}-type Pokémon with an attack stat of {self.attack} and base experience of {self.base_exp}."
+
+def get_pokemon(name):
+    url = f"https://pokeapi.co/api/v2/pokemon/{name.lower()}"
+    response = requests.get(url)
+    data = response.json()
+
+    pokemon_obj = Pokemon(
+        data["name"],
+        data["types"][0]["type"]["name"],
+        data["stats"][1]["base_stat"],
+        data["base_experience"]
+    )
+    return pokemon_obj
+
+class Pokedex:
     def __init__(self):
         self.pokemons = {}
 
     def add_pokemon(self, pokemon):
         self.pokemons[pokemon.name] = pokemon
 
-    def display_pokemon(self):
+    def display_all(self):
         for pokemon in self.pokemons.values():
             print(pokemon.get_pokemon_info())
+
+# Проверка работы:
+my_pokedex = Pokedex()
+pikachu = get_pokemon("pikachu")
+my_pokedex.add_pokemon(pikachu)
+
+my_pokedex.display_all()
